411a67ecde6b85b852e8f7cf562ec56637ad06c0
bugname7 <valisher2106@gmail.com>
2024-09-27 23:16:14 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// 1-masala
+
+// let number1 = +prompt("sonni kiritin");
+// if (number1 > 0) {
+//   console.log("musbat");
+// } else {
+//   console.log("manfiy");
+// }
+
+// 2-masala
+// let number2 = +prompt("sonni kirit");
+// if (number2 % 2 == 0) {
+//   console.log("juft");
+// } else {
+//   console.log("toq");
+// }
+// 3-masala
+// let number3 = +prompt("birinchi son");
+// let number4 = +prompt("ikkinchi son");
+// let resualt = "";
+// if (number3 > number4) {
+//   console.log("katta");
+// } else if (number3 < number4) {
+//   console.log("kichik");
+// } else {
+//   console.log("teng");
+// }
+// 4-masala
+// let uzunlik = +prompt("balandlik");
+// let eni = +prompt(" eni ");
+
+// if (uzunlik == eni) {
+//   console.log("kvadrat");
+// } else {
+//   console.log("kvadrat emas");
+// }
+// 5-masala
+// let harorat = +prompt("haroratni kiriting");
+// if (harorat > 0) {
+//   console.log("Yozgi");
+// } else if (harorat < 0) {
+//   console.log("qishki");
+// }
+// 6-masala
+// let baho = +prompt("bahoingizni kiriting");
+
+// if (baho == 5) {
+//   console.log("alo");
+// } else if (4 == baho <= 5) {
+//   console.log("Yaxshi");
+// } else if (3 == baho < 4) {
+//   console.log("Qoniqarli");
+// } else if (2 == baho < 3) {
+//   console.log("Yomon");
+// } else if (baho == 1) {
+//   console.log("haydaldin ukam");
+// }
+
+// 7-masala
+let Number1 = +prompt("biriinchi son ");
+let Number2 = +prompt("ikkinci son ");
+let Number3 = +prompt("uchinchi son ");
+let katta = Number1;
+
+if (Number1 >= Number2) {
+ 
+}
+else if (res > Number3){
+  res = 
+}
